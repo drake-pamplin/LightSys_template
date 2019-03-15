0c180b5a37f7b9ced99b5fd7eb81ee24290a183e
Drake <drake.pamplin@gmail.com>
2019-03-15 12:22:59 -0700

    Added Ben's Media Player
    Added Ben's media player and encountered one bug. Ben's working on it now.

--- app/src/main/java/com/example/card_menu/image_viewer.java
+++ app/src/main/java/com/example/card_menu/image_viewer.java
@@ -1,7 +1,11 @@
 package com.example.card_menu;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.media.MediaPlayer;
+import android.net.wifi.WifiManager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,9 +14,43 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
 
+import java.io.IOException;
+
 public class image_viewer extends AppCompatActivity {
 
     ImageView imgDisplay;
+
+    String[] booklink = {
+            "A64560",
+            "A65688",
+            "A65689",
+            "A65761",
+            "A65762",
+            "A65763",
+            "A65764",
+            "A65765",
+            "A65766"
+    };
+
+    MediaPlayer mediaPlayer;
+
+    public String link ="https://5fish.mobi/";
+    public Menu menu;
+    private WifiManager wifiManager;
+    boolean wifi = false;
+    boolean enter = true;
+    boolean go = false;
+
+    private BroadcastReceiver wifiStateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int wifiStateExtra = intent.getIntExtra(WifiManager.EXTRA_WIFI_STATE,
+                    WifiManager.WIFI_STATE_UNKNOWN);
+            if (wifiStateExtra == WifiManager.WIFI_STATE_ENABLED) {
+                wifi = true;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,12 +75,37 @@ public class image_viewer extends AppCompatActivity {
 
         imgDisplay = findViewById(R.id.imgDisplay);
         imgDisplay.setImageDrawable(c.getResources().getDrawable(c.getResources().getIdentifier("book_" + book + "_lesson_" + lesson, "drawable", c.getPackageName())));
+
+        int book_I = Integer.parseInt(book);
+        String book_S = booklink[Integer.parseInt(book)];
+
+        int lesson_I = Integer.parseInt(lesson);
+        String lesson_S;
+        if (lesson_I < 10 && book_I != 2) {
+            lesson_S = "00" + lesson;
+        } else {
+            lesson_S = "0" + lesson;
+        }
+
+        link = link + book_S + "/low/" + book_S + "-" + lesson_S + ".mp3";
+
+        wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        IntentFilter intentFilter = new IntentFilter(WifiManager.WIFI_STATE_CHANGED_ACTION);
+        registerReceiver(wifiStateReceiver, intentFilter);
     }
 
     //create overflow menu
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.mp3_view_menu, menu);
+        if(wifi)
+        {
+            getMenuInflater().inflate(R.menu.mp3_view_menu, menu);
+        }
         return true;
     }
 
@@ -50,17 +113,82 @@ public class image_viewer extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
-        Intent intent = new Intent(this, mp3_access.class);
-
         switch (item.getItemId()) {
-            case R.id.action_mp3:
-                this.startActivity(intent);
+            case R.id.action_play:
+                play();
+
                 return true;
+
+            case R.id.action_restart:
+                restart();
+                return true;
+
 
             default:
                 //If we got here, the user's action was not recognized.
                 //Invoke the superclass to handle it.
                 return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void onStop() {
+        if (!enter || go)
+        {
+            enter = true;
+            go = false;
+            mediaPlayer.release();
+        }
+        unregisterReceiver(wifiStateReceiver);
+        super.onStop();
+    }
+
+    public void play() {
+        if (enter) {
+            enter = false;
+            go = true;
+            mediaPlayer = new MediaPlayer();
+            try {
+                mediaPlayer.setDataSource(link);
+
+                mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                    @Override
+                    public void onPrepared(MediaPlayer mp) {
+                        mp.start();
+                    }
+                });
+                mediaPlayer.prepareAsync();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                public void onCompletion(MediaPlayer mp) {
+                    mediaPlayer.release(); // finish current activity
+                    enter = true;
+                    go = false;
+                }
+            });
+        }
+        else {
+            if (go) {
+                go = false;
+                mediaPlayer.pause();
+            }
+            else {
+                go = true;
+                mediaPlayer.start();
+            }
+        }
+    }
+
+    public void restart() {
+        if (!enter)
+        {
+            enter = true;
+            go = true;
+            mediaPlayer.release();
+            play();
         }
     }
 }
